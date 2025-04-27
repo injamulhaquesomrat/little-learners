76c5b0f507d1ef8157aacf8facaa777e7160e312
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-04-27 18:19:18 +0600

    refactor: update favicon and integrate Google Fonts; remove unused assets and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-// import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Button } from "./components/ui/button";
 
 function App() {
@@ -10,12 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Little Learners</h1>
       </div>
       <h1>Vite + React</h1>
       <Button>Shadcn</Button>
