f839b657fb6b9826b7f5de57883aa249a933fab1
haquesomrat <ihsomrat.official@gmail.com>
2025-04-28 22:28:05 +0600

    feat: implement Benefits section with SectionHeading component; add Badge component for enhanced styling

--- src/components/Homepage/Benefits.tsx
+++ src/components/Homepage/Benefits.tsx
@@ -1,5 +1,17 @@
+import SectionHeading from "../shared/SectionHeading";
+
 const Benefits = () => {
-  return <section>Benefits</section>;
+  return (
+    <section className="pb-20 xl:pb-[150px] 3xl:pb-[200px]">
+      <SectionHeading
+        badgeText={"Children Deserve Bright Future"}
+        title={"Our Benefits"}
+        subHeading={`With a dedicated team of experienced educators, state-of-the-art
+        facilities, and a comprehensive curriculum, we aim to lay a strong
+        foundation for your child's future.`}
+      />
+    </section>
+  );
 };
 
 export default Benefits;
--- src/components/shared/SectionHeading.tsx
+++ src/components/shared/SectionHeading.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { Badge } from "../ui/badge";
 
-const SectionHeading = () => {
-    return (
-        <div>
-            Section heading
-        </div>
-    );
+const SectionHeading = ({
+  badgeText,
+  title,
+  subHeading,
+}: {
+  badgeText: string;
+  title: string;
+  subHeading: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center text-center gap-2.5 md:gap-3 lg:gap-3.5 3xl:gap-[19px] pb-[50px] xl:pb-20 3xl:pb-[100px]">
+      <Badge
+        variant={"neutral"}
+        className="text-[var(--grey-20)] px-3.5 py-2 3xl:px-5 3xl:py-2.5 bg-white text-sm xl:text-base 3xl:text-lg"
+      >
+        {badgeText}
+      </Badge>
+
+      <h2 className="text-[38px] md:text-[42px] lg:text-5xl 3xl:text-[58px] font-bold font-raleway text-ring leading-[1.3]">
+        {title}
+      </h2>
+
+      <p className="text-[var(--grey-20)] font-medium leading-[1.5] md:w-[700px] xl:w-[880px] 3xl:w-[992px] 3xl:text-xl">
+        {subHeading}
+      </p>
+    </div>
+  );
 };
 
 export default SectionHeading;
