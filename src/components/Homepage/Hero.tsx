a5606df2c5667d96dc9341a42496dcf853bc30b9
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-04-28 13:59:16 +0600

    feat: enhance Hero component layout and styling; add text formatting and padding adjustments

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section className="w-full flex flex-col gap-10">
+    <section className="w-full flex flex-col gap-10 pb-20">
       {/* start::hero image container */}
       <div className="w-full pt-[50px]">
         <img
@@ -12,11 +12,14 @@ const Hero = () => {
       {/* end::hero image container */}
 
       {/* start::hero text container */}
-      <div>
-        <div>
-          <span>Welcome to Little Learners Academy</span>
-          <h1>
-            Where Young Minds Blossom and <span>Dreams Take Flight.</span>
+      <div className="text-center">
+        <div className="pb-10">
+          <p className="font-medium border-b-[2px] border-border leading-loose w-fit mx-auto mb-2.5 text-grey-15">
+            Welcome to Little Learners Academy
+          </p>
+          <h1 className="font-raleway text-[30px] font-extrabold text-ring leading-[1.3] mb-[1.125rem]">
+            Where Young Minds Blossom and{" "}
+            <span className="text-main">Dreams Take Flight.</span>
           </h1>
           <p>
             Our kinder garden school provides a nurturing and stimulating
@@ -24,20 +27,20 @@ const Hero = () => {
             Join us as we embark on an exciting educational journey together!
           </p>
         </div>
-        <div>
+        <div className="p-10 border-[2px] rounded-xl grid space-y-5 bg-secondary-background">
           <div>
-            <h3>+7000</h3>
-            <p>Students Passed Out</p>
+            <h3 className="text-gray-15 text-[34px] font-extrabold">+7000</h3>
+            <p className="font-medium text-grey-15">Students Passed Out</p>
           </div>
-          <hr />
+          <hr className="lg:hidden border-b" />
           <div>
-            <h3>+37</h3>
-            <p>Awards & Recognitions</p>
+            <h3 className="text-gray-15 text-[34px] font-extrabold">+37</h3>
+            <p className="font-medium text-grey-15">Awards & Recognitions</p>
           </div>
-          <hr />
+          <hr className="lg:hidden border-b" />
           <div>
-            <h3>+15</h3>
-            <p>Experience Educators</p>
+            <h3 className="text-gray-15 text-[34px] font-extrabold">+15</h3>
+            <p className="font-medium text-grey-15">Experience Educators</p>
           </div>
         </div>
       </div>
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 
 const Root = () => {
   return (
-    <div className="bg-background font-outfit">
+    <div className="bg-background font-outfit text-[var(--grey-20)]">
       <Navbar />
       <Outlet />
     </div>
