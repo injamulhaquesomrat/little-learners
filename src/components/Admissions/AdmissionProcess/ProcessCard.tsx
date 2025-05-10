255843603f45e460fb127cdaf5613c98561dd85c
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-05-10 20:59:38 +0600

    feat: create admissionProcessItems data structure and refactor AdmissionProcessContent and ProcessCard components to utilize it

--- src/components/Admissions/AdmissionProcess/ProcessCard.tsx
+++ src/components/Admissions/AdmissionProcess/ProcessCard.tsx
@@ -10,19 +10,23 @@ import { admissionProcessTypes } from "@/types/admissionProcessTypes";
 const ProcessCard = ({ process }: { process: admissionProcessTypes }) => {
   return (
     <div className="flex flex-col gap-10 3xl:gap-20 relative h-full">
+      {/* start::process number card */}
       <div className="w-fit h-fit mx-auto sm:mx-0 relative">
         <div className="font-raleway bg-white text-[40px] xl:text-[44px] 3xl:text-[60px] font-extrabold text-[var(--grey-10)] leading-[1.3] px-6 py-2 xl:py-2.5 border-2 border-border shadow-light-shadow rounded-[10px]">
           0{process?.id}
         </div>
         <img
           className="absolute -bottom-3 left-1/2 -translate-x-1/2 z-20"
-          src="/public/assets/icons/history/circle-ring.svg"
+          src="/assets/icons/history/circle-ring.svg"
           alt="circle ring"
         />
         <div className="py-1.5 bg-[var(--orange-80)] absolute -bottom-[27px] 3xl:-bottom-[51px] left-1/2 -translate-x-1/2 rotate-90">
           <div className="w-10 3xl:w-18 bg-black h-[2px] "></div>
         </div>
       </div>
+      {/* end::process number card */}
+
+      {/* start::process card */}
       <Card className="p-[30px] xl:p-10 3x:p-[50px] bg-white gap-3.5 xl:gap-4 3xl:gap-5 h-full">
         <CardHeader className="p-0 gap-0">
           <CardTitle className="font-raleway text-[26px] xl:text-[30px] 3xl:text-[40px] font-extrabold text-[var(--grey-10)] leading-[1.3]">
@@ -41,6 +45,7 @@ const ProcessCard = ({ process }: { process: admissionProcessTypes }) => {
       >
         <div className="w-9 bg-black h-[2px] "></div>
       </div>
+      {/* end::process number card */}
     </div>
   );
 };
---
+++ src/fakedata/admissionProcessItems.tsx
@@ -0,0 +1,38 @@
+export const admissionProcessItems = [
+  {
+    id: 1,
+    name: "Inquiry",
+    details:
+      "Submit an inquiry form through our website or contact our admissions office to express your interest in Little Learners Academy.",
+  },
+  {
+    id: 2,
+    name: "School Tour",
+    details:
+      "Schedule a personalized school tour to explore our campus, meet our staff, and gain insights into our nurturing learning environment.",
+  },
+  {
+    id: 3,
+    name: "Application Form",
+    details:
+      "Complete the application form and provide the required documents, including your child's birth certificate, medical records, and any previous academic records (if applicable).",
+  },
+  {
+    id: 4,
+    name: "Parent Interview",
+    details:
+      "We value parent engagement, and a meeting with our admissions team allows us to understand your child's needs and ensure Little Learners Academy aligns with your family's expectations.",
+  },
+  {
+    id: 5,
+    name: "Student Assessment",
+    details:
+      "For certain age groups, a student assessment may be conducted to understand their developmental progress and ensure the best placement.",
+  },
+  {
+    id: 6,
+    name: "Acceptance",
+    details:
+      "Once the admission process is complete, you will receive an official acceptance letter from Little Learners Academy.",
+  },
+];
