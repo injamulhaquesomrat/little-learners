74f82d19520eab6781d93a7a46f6524ecd88b1a4
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-05-14 17:39:17 +0600

    fix: update icon paths in extracurricularItems and studentSupportItems to remove outdated references

--- src/fakedata/studentSupportItems.tsx
+++ src/fakedata/studentSupportItems.tsx
@@ -1,21 +1,21 @@
 export const studentSupportItems = [
   {
     id: "ss-1",
-    icon: "/public/assets/icons/student-support/counseling-icon.svg",
+    icon: "/assets/icons/student-support/counseling-icon.svg",
     title: "Counseling",
     description:
       "Professional counselors offer guidance and support to students, addressing their emotional and social well-being.",
   },
   {
     id: "ss-2",
-    icon: "/public/assets/icons/student-support/learning-support-icon.svg",
+    icon: "/assets/icons/student-support/learning-support-icon.svg",
     title: "Learning Support",
     description:
       "Our educators provide additional assistance to students who may require extra support in their academic journey.",
   },
   {
     id: "ss-3",
-    icon: "/public/assets/icons/student-support/parent-teacher-collaboration-icon.svg",
+    icon: "/assets/icons/student-support/parent-teacher-collaboration-icon.svg",
     title: "Parent-Teacher Collaboration",
     description:
       "We foster a strong partnership with parents to ensure seamless communication and mutual support in a child's development.",
